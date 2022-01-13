105dcf1cd1852664ddb144edf3dc685f1af622e9
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-13 13:15:46 +0200

    Remove unsed code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
