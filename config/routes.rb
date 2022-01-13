47cf3244b3896bd978f5e4b0f8f42d468b2ad776
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-13 10:22:44 +0200

    Add simple placeholders for my views with plain HTML

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
   # Defines the root path route ("/")
-  # root "articles#index"       
+  # root "articles#index"
 end
