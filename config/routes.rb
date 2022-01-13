109e9b474893a870222c2810ee09c94aa87e6882
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-13 09:59:23 +0200

    Delete users and posts then generate controller using command line

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   resources :users, only: [:index, :show] do
