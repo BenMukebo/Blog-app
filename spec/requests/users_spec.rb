a99c77ef079967f9f6267cc0ba711edffbddef75
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-14 08:45:48 +0200

    Check response status, correct template, response body of the users controller show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,6 +20,22 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+  describe 'GET /show' do
+    before(:each) { get user_path(32) }
+  
+    it 'should returns http 200 success' do
+      # get '/users/show'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should render the show template ' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include the word: Show the user details by ID' do
+      expect(response.body).to include('Show the user details by ID')
+    end
+  end
 end
 
 # RSpec.describe "users controller", type: :request do
@@ -40,5 +56,20 @@ end
 #       end
 #     end
 
+#     describe "GET /users/:id" do
+#       before(:example) { get "/users/1" }
+
+#       it "should return 200 http_status_code" do
+#         expect(response).to have_http_status(:ok)
+#       end
+
+#       it "should render the correct template" do
+#         expect(response).to render_template(:show)
+#       end
+
+#       it "should render the correct text in the template" do
+#         expect(response.body).to include('Show the user details by ID')
+#       end
+#     end
 #   end
 # end
