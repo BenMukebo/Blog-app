6fdd6d92b6b63b6e094a79f1e136513ed574cdef
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-14 08:56:23 +0200

    Remove commented code

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -29,39 +29,3 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 end
-
-
-# RSpec.describe "Posts controller", type: :request do
-#   describe "GET posts routes" do
-#     describe "GET /users/:user_id/posts" do
-#       before(:each) { get "/users/1/posts" }
-
-#       it "should return 200 http_status_code" do
-#         expect(response).to have_http_status(:success)
-#       end
-#       it 'should render the correct template' do
-#         expect(response).to render_template(:index)
-#       end
-#       it 'should have the text: show the posts of a user' do
-#         expect(response.body).to include('show the posts of a user')
-#       end
-#     end
-
-#     describe "GET /users/:user_id/posts/:id" do
-#       before(:each) { get "/users/1/posts/3" }
-
-#       it "should return 200 http_status_code" do
-#         expect(response).to have_http_status(:ok)
-#       end
-
-#       it "should render the correct template" do
-#         expect(response).to render_template(:show)
-#       end
-
-#       it "should render the correct text in the template" do
-#         expect(response.body).to include('show posts details')
-#       end
-#     end
-#   end
-# end
-
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -36,39 +36,3 @@ RSpec.describe 'Users', type: :request do
     end
   end
 end
-
-# RSpec.describe "users controller", type: :request do
-#   describe "GET user routes" do
-#     describe "GET /users" do
-#       before(:example) { get "/users" }
-
-#       it "should return 200 http_status_code" do
-#         expect(response).to have_http_status(:ok)
-#       end
-
-#       it "should render the correct template" do
-#         expect(response).to render_template(:index)
-#       end
-
-#       it "should render the correct text in the template" do
-#         expect(response.body).to include('List of users is here')
-#       end
-#     end
-
-#     describe "GET /users/:id" do
-#       before(:example) { get "/users/1" }
-
-#       it "should return 200 http_status_code" do
-#         expect(response).to have_http_status(:ok)
-#       end
-
-#       it "should render the correct template" do
-#         expect(response).to render_template(:show)
-#       end
-
-#       it "should render the correct text in the template" do
-#         expect(response.body).to include('Show the user details by ID')
-#       end
-#     end
-#   end
-# end
