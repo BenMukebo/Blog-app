57426c5d13ec31373b7675c08f2db66c9531cba1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-15 03:25:26 +0200

    Fix linters errors

--- db/migrate/20220114231638_create_comments.rb
+++ db/migrate/20220114231638_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-    t.string :text
+      t.string :text
 
-    t.timestamps
+      t.timestamps
+    end
   end
 end
