f8a85955c3cd0b4cc29b94bf52d2a3e04d7b125e
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-19 11:30:01 +0200

    Include some additional methods to the Users, Posts, Comments, Likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many: posts
   has_many: comments
   has_many: likes
+  # has_many :posts, foreign_key: 'author_id'
+  # has_many :comments, foreign_key: 'author_id'
+  # has_many :likes, foreign_key: 'author_id'
+
+  def most_recent_posts
+    posts.order('created_at DESC').limit(3)
+  end
 end
