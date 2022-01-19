4a3afacf4d0c63bd6117dc109c893ffddc095173
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-19 12:36:33 +0200

    Links foreign_key to the table users

--- db/migrate/20220119075348_create_likes.rb
+++ db/migrate/20220119075348_create_likes.rb
@@ -1,10 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      # t.references :author, foreign_key: { to_table: 'users' }, null: false
-      # t.references :post, foreign_key: true, null: false
-      t.references :author, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }, null: false
+      t.references :post, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/migrate/20220119075622_create_posts.rb
+++ db/migrate/20220119075622_create_posts.rb
@@ -1,13 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: true
-      # t.references :author, foreign_key: { to_table: 'users'}, null: false
+      t.references :author, foreign_key: { to_table: 'users'}, null: false
       t.string :title
       t.text :text
       t.integer :commentsCounter
       t.integer :likesCounter
-      # t.integer :postsCounter
 
       t.timestamps
     end
