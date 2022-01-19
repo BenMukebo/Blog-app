30d7d6594c436f17f8a4bc79a5ef7d7ac470110a
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-19 18:15:46 +0200

    Make private the update_posts_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
+  private
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
@@ -13,4 +15,3 @@ class Post < ApplicationRecord
     comments.order('created_at DESC').limit(5)
   end
 end
-
