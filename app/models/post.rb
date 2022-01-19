4767fdb6155bb575836917f9965e1d1c032cec49
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-19 18:08:47 +0200

    Remove commented line left in the post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,19 +14,3 @@ class Post < ApplicationRecord
   end
 end
 
-# class Post < ApplicationRecord
-#   belongs_to :author, class_name: 'User'
-#   has_many :comments
-#   has_many :likes
-
-#   after_save :update_post_counter
-
-#   def update_post_counter
-#     author.increment!(:posts_counter)
-#   end
-
-#   def five_recent_comments
-#     # comments.order(created_at: :desc).limit(5)
-#     comments.order('created_at DESC').limit(5)
-#   end
-# end
