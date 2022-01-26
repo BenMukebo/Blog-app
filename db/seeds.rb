088914f44d046185171d215160199eff8340ff70
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-26 22:40:13 +0200

    Add style to the second page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 lorem_bio = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, labore nam? Perferendis hic,
-  esse quia in corrupti doloremque laborum, omnis quae placeat facere aliquid amet excepturi.'
+  esse quia in corrupti doloremque laborum, omnis quae placeat facere aliquid amet excepturiquia in corrupti
+  doloremqueamet excepturi laborum,amet excepturi omnis quae placeat facere aliquid amet excepturi.'
+
 image1 = 'https://user-images.githubusercontent.com/79058364/150398546-6d9d32a8-dc4c-4e26-a6ff-0ff0b4eafe9f.jpg'
 image2 = 'https://user-images.githubusercontent.com/79058364/150398981-d7619766-6bf3-4878-9bc7-b0ef2d33f815.jpg'
 image3 = 'https://user-images.githubusercontent.com/79058364/150375449-6871e044-0e98-4378-8651-08d29f8adbf8.jpg'
@@ -17,8 +19,8 @@ image5 = 'https://user-images.githubusercontent.com/79058364/150398570-de44d578-
 user1 = User.create!(name: 'John Doe', photo: image1, bio: "#{lorem_bio}- John Doe", posts_counter: 0)
 user2 = User.create!(name: 'Michal Smith ', photo: image2, bio: "#{lorem_bio}- Michal Smith", posts_counter: 0)
 user3 = User.create!(name: 'Julia Lisa', photo: image3, bio: "#{lorem_bio}- Danny Dril", posts_counter: 0)
-user4 = User.create!(name: 'Oliver Queen', photo: image4, bio: "#{lorem_bio}- Oliver Queen", posts_counter: 0)
-user5 = User.create!(name: 'Barry alen', photo: image5, bio: "#{lorem_bio}- Barry alen", posts_counter: 0)
+user4 = User.create!(name: 'Gomez Lilia', photo: image4, bio: "#{lorem_bio}- Oliver Queen", posts_counter: 0)
+user5 = User.create!(name: 'Barry Alen', photo: image5, bio: "#{lorem_bio}- Barry alen", posts_counter: 0)
 
 lorem_one = 'Donec dapibus consequat tellus a pulvinar. Fusce auctor rhoncus orci a semper. Sed interdum accumsan.'
 lorem_two = 'Mauris hendrerit odio eu quam dapibus, sit amet malesuada quam pulvinar venenatis ex, eu tristique orci.'
@@ -36,12 +38,14 @@ post6 = user3.posts.create!(title: 'Agriculture', text: lorem_two, comments_coun
 post7 = user4.posts.create!(title: 'Arts', text: lorem_one, comments_counter: 0, likes_counter: 0)
 
 user3.comments.create!(text: 'Well Done 👏👏👏', post: post1)
+user2.comments.create!(text: 'Great like post ✔️✔️  ', post: post1)
 user5.comments.create!(text: 'really nice post brother', post: post2)
 user1.comments.create!(text: 'This is a great Read', post: post2)
 user2.comments.create!(text: 'Love 💕 it Aswell !!!', post: post2)
 user4.comments.create!(text: 'Love 💓 it!!!', post: post4)
 user5.comments.create!(text: 'We still need one more post😢', post: post4)
 user3.comments.create!(text: 'This is a terific post ', post: post5)
+user3.comments.create!(text: 'Well say,♻️♻️ that is realy true ', post: post5)
 user3.comments.create!(text: 'Great look on this article', post: post7)
 
 user2.likes.create!(post: post1)
@@ -50,5 +54,9 @@ user1.likes.create!(post: post3)
 user3.likes.create!(post: post3)
 user5.likes.create!(post: post3)
 user1.likes.create!(post: post4)
+user4.likes.create!(post: post4)
+user4.likes.create!(post: post5)
 user2.likes.create!(post: post5)
 user2.likes.create!(post: post6)
+user2.likes.create!(post: post6)
+user5.likes.create!(post: post7)
