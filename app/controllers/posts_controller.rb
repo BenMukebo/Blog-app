61607981ae0ece9d38994c0071bf549aca34719d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-27 21:34:38 +0200

    Implementing the last section

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    # @comments = @posts.comments
   end
   
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments = @post.comments
   end
 end
