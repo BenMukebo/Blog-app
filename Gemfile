ef1c8e9f776668dd390cfb55a8926e30d272269c
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-28 23:17:42 +0200

    Implement the add comment functionality

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'bootsnap', require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
+gem "font-awesome-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,36 @@
 class CommentsController < ApplicationController
-  def new
 
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @post.comments.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @user.comments.new(text: comment_params[:text], post_id: @post.id, author_id: @user.id)
+    put
+    if @comment.save
+       redirect_to user_post_path(@user, @post)
+    else 
+      render 'new'
+    end
+  end
 
+  # def create
+  #   @comment = Comment.new(comment_params)
+  #   @user = User.find(params[:user_id])
+  #   @comment.user_id = params[:user_id]
+  #   @comment.post_id = params[:post_id]
+  #   if @comment.save
+  #     redirect_to user_post_path(@comment.user)
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
