87bb3e864ed4d05dae3660170fbb513c4a05a6bf
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-28 14:31:25 +0200

    Add new and create actions to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # post 'users/:user_id/posts/:id/create_comment', to: 'comments#create', as: 'create_comment'
-  # post 'users/:user_id/posts/:id/create_like', to: 'likes#create', as: 'like_comment'
-
   root to: 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
-      # post 'add_comment', to: 'comments#create', on: :member
-      # post 'add_like', to: 'likes#create', on: :member
+    resources :posts, only: %i[index show   new create] do
+      resources :comments, only: %i[new create]
     end
   end
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
-
