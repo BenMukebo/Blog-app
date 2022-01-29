ed585c17f6008bc0652074d3e166d545ada77765
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-01-29 13:58:34 +0200

    Add validations for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: { message: 'Name must not be blank' }
+  validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal: 0
+
+
   def most_recent_posts
     posts.order('created_at DESC').limit(3)
   end
