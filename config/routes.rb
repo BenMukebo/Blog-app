5e5ed97bc12b85da2097cc6438cd6eacd52f40c1
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-01 23:23:51 +0200

    Add destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create]
       resources :likes, only: %i[create]
     end
