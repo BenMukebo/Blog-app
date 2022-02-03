cf792c82b4f725d0d3924114bf49cd4931f08a54
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-03 18:45:13 +0200

    I can see the user's profile picture

--- spec/features/user_post_index_spec.rb
+++ spec/features/user_post_index_spec.rb
@@ -1,9 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe 'Post Index Page Features', type: :feature, js: true do
-  before(:all) do
-    # Selenium::WebDriver.logger.level = :debug
-    Capybara.current_driver = :headless_chrome
-    Capybara.javascript_driver = :headless_chrome
+  before(:each) do
+    # visit new_user_registration_path
+    # visit new_user_session_path
+    visit user_session_path
+    @photo = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/58/ballot-box-with-x_2612.png'
+
+    @user1 = User.create!(
+      name: 'John Doe', bio: 'This is John Doe bio', photo: @photo, email: 'joe@mail.com',
+      password: '123456', posts_counter: 6
+    )
+    @user2 = User.create!(
+      name: 'Michal Smith', bio: 'This is Michal Smith bio', photo: @photo, email: 'michal@mail.com', password: '123456',
+      posts_counter: 3
+    )
+
+    fill_in 'Email', with: 'mike@gmail.com'
+    fill_in 'Password', with: 'mikemike'
+    click_button 'Log in'
+
+    # Posts
+
+    @post1 = @user1.posts.create!(
+      title: 'Education', text: 'This is first Educat post!', comments_counter: 0, likes_counter: 0
+    )
+    @post2 = @user1.posts.create!(
+      title: 'Sport', text: 'This is second Sport post!', comments_counter: 0, likes_counter: 0
+    )
+    @post3 = @user2.posts.create!(
+      title: 'Bussiness', text: 'This is third!', comments_counter: 0, likes_counter: 0
+    )
+
+    # Comments
+
+    @user1.comments.create!(text: 'Really nice post', post: @post1)
+    @user2.comments.create!(text: 'Amezing', post: @post1)
+    @user2.comments.create!(text: 'Fantastic', post: @post2)
+
+    @user1.likes.create!(post: @post1)
+    @user1.likes.create!(post: @post2)
+    @user2.likes.create!(post: @post1)
+    @user2.likes.create!(post: @post3)
+
+    visit user_posts_path(@user1)
   end
+
+  describe 'I can see' do
+    it 'the user\'s profile picture' do
+      image = page.find('img')['src']
+      expect(image).to eql @photo
+    end
+
+
+    
 end
